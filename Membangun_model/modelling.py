63e505cd04125f38b92b925ff0ad039ec31a24fb
Chandra <adityanugraha7251@gmail.com>
2025-11-27 16:44:29 +0800

    idk

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -1,56 +1,52 @@
-
 import pandas as pd
 import numpy as np
-
-from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.svm import SVC
-
-import os
-import mlflow as mlf
 import math
+import mlflow
+import mlflow.sklearn
 
-my_path = '/content/aapl.us.txt_preprocessing.csv'
-df = pd.read_csv(my_path)
-df.head()
+from sklearn.svm import SVR
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 
-data = df.filter(['Close_norm'])
+# ==== Load dataset ====
+df = pd.read_csv("aapl.us.txt_preprocessing.csv")
 
-dataset = data.values
+# Menggunakan fitur Close_norm
+dataset = df["Close_norm"].values.reshape(-1, 1)
 
-training_data_len = math.ceil(len(dataset) * .8)
+# ==== Windowing function ====
+def create_window(data, window_size=60):
+    X, y = [], []
+    for i in range(window_size, len(data)):
+        X.append(data[i-window_size:i, 0])
+        y.append(data[i, 0])
+    return np.array(X), np.array(y)
 
-train_data = dataset[0:training_data_len, :]
+# ==== Split Train/Test ====
+training_data_len = math.ceil(len(dataset) * 0.8)
 
-X_train = []
-y_train = []
+train_data = dataset[:training_data_len]
+test_data  = dataset[training_data_len - 60:]
 
-#windowing
-def split_data(data,x,y):
-  for i in range(60, len(data)):
-    x.append(data[i-60:i,0])
-    y.append(data[i,0])
+X_train, y_train = create_window(train_data, 60)
+X_test,  y_test  = create_window(test_data, 60)
 
-split_data(train_data, X_train,y_train)
+# ==== MLflow setup ====
+mlflow.set_tracking_uri("mlruns")  # lokal, tidak perlu server
+mlflow.set_experiment("stock_prediction")
 
-X_train, y_train = np.array(X_train), np.array(y_train)
+with mlflow.start_run():
+    mlflow.autolog()
 
-mlf.set_tracking_uri(uri="http://127.0.0.1:5000/")
+    model = SVR(kernel="rbf", C=100, gamma=0.1)
+    model.fit(X_train, y_train)
 
-test_data = dataset[training_data_len - 60:, :]
+    # ==== Evaluation ====
+    predictions = model.predict(X_test)
+    rmse = np.sqrt(mean_squared_error(y_test, predictions))
 
-X_test = []
-y_test = dataset[training_data_len:, :]
+    print("RMSE:", rmse)
 
-split_data(test_data,X_test,[])
+    mlflow.log_metric("RMSE", rmse)
 
-with mlf.start_run():
-  model = SVC()
-
-  mlf.autolog()
-
-  mlf.sklearn.log_model(
-      sk_model=model,
-      artifact_path="model",
-  )
-  model.fit(X_train, y_train)
-  accuracy = model.score(X_test,y_test)
+    mlflow.sklearn.log_model(model, "model")
