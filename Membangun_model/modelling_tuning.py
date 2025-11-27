0cf1bba247983812aaebfa806ebf402aae6ed856
Chandra <adityanugraha7251@gmail.com>
2025-11-27 16:44:29 +0800

    idk

---
+++ Membangun_model/modelling_tuning.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import math
+
+import mlflow
+import mlflow.sklearn
+
+import keras
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
